c6f984f3140150aed6ef5648520e1332b506c319
Prasanna03-git <komakula3@gmail.com>
2025-02-12 22:14:11 -0500

    commit tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "client_id" {
+    description = "The Client ID of the Azure Service Principal"
+    type        = string
+}
+variable "client_secret" {
+    description = "The Client Secret of the Azure Service Principal"
+    type        = string
+}
+variable "tenant_id" {
+    description = "The Tenant ID of the Azure Active Directory"
+    type        = string
+}
+variable "subscription_id" {
+    description = "The Azure Subscription ID"
+     type        = string
+}
